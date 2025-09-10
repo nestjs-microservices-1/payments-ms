311ae633ff4191ef540962f87d2bc0648d1e0935
ph <pio.huisa@gmail.com>
2025-09-10 18:54:02 -0500

    feat: implementation stripeWebhoook function

--- src/payments/payments.service.ts
+++ src/payments/payments.service.ts
@@ -2,13 +2,14 @@ import { Injectable } from '@nestjs/common';
 import { envs } from 'src/config';
 import Stripe from 'stripe';
 import { PaymentSessionDto } from './dto/payment-session.dto';
+import { Request, Response } from 'express';
 
 @Injectable()
 export class PaymentsService {
   private readonly stripe = new Stripe(envs.STRIPE_SECRET_KEY);
 
   async createPaymentSession(paymentSessionDto: PaymentSessionDto) {
-    const { currency, items } = paymentSessionDto;
+    const { currency, items, orderId } = paymentSessionDto;
 
     const lineItems = items.map((item) => ({
       price_data: {
@@ -22,13 +23,43 @@ export class PaymentsService {
     }));
     const session = await this.stripe.checkout.sessions.create({
       payment_intent_data: {
-        metadata: {},
+        metadata: {
+          orderId,
+        },
       },
       line_items: lineItems,
       mode: 'payment',
-      success_url: `http://localhost:3003/payments/success`,
-      cancel_url: `http://localhost:3003/payments/cancel`,
+      success_url: envs.STRIPE_SUCCESS_URL,
+      cancel_url: envs.STRIPE_CANCEL_URL,
     });
     return session;
+  }
+
+  async stripeWebhook(req: Request, res: Response) {
+    const signature = req.headers['stripe-signature'] as string;
+    const endpointSecret = envs.STRIPE_ENDPOINT_SECRET;
+    let event: Stripe.Event;
+    try {
+      event = this.stripe.webhooks.constructEvent(
+        req['rawBody'],
+        signature,
+        endpointSecret,
+      );
+    } catch (error) {
+      res.status(400).send(`Webhook Error: ${error.message}`);
+      return;
+    }
+    switch (event.type) {
+      case 'charge.succeeded':
+        const chargeSucceded = event.data.object;
+        console.log({
+          metadata: chargeSucceded.metadata,
+        });
+        break;
+      // ... handle other event types
+      default:
+        console.log(`Unhandled event type ${event.type}`);
+    }
+    return res.status(200).json({ signature });
   }
 }
