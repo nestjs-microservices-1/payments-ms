372323eda561bacaba938b926c3c78102039f2c2
ph <pio.huisa@gmail.com>
2025-09-11 15:13:15 -0500

    feat: add microservices

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -7,6 +7,8 @@ interface EnvVars {
   STRIPE_SUCCESS_URL: string;
   STRIPE_CANCEL_URL: string;
   STRIPE_ENDPOINT_SECRET: string;
+
+  NATS_SERVERS: string[];
 }
 const envsSchema = joi
   .object({
@@ -15,11 +17,14 @@ const envsSchema = joi
     STRIPE_SUCCESS_URL: joi.string().required(),
     STRIPE_CANCEL_URL: joi.string().required(),
     STRIPE_ENDPOINT_SECRET: joi.string().required(),
+
+    NATS_SERVERS: joi.array().items(joi.string()).required(),
   })
   .unknown(true);
 
 const { error, value } = envsSchema.validate({
   ...process.env,
+  NATS_SERVERS: process.env.NATS_SERVERS?.split(','),
 });
 
 if (error) {
@@ -33,4 +38,6 @@ export const envs = {
   STRIPE_SUCCESS_URL: envVars.STRIPE_SUCCESS_URL,
   STRIPE_CANCEL_URL: envVars.STRIPE_CANCEL_URL,
   STRIPE_ENDPOINT_SECRET: envVars.STRIPE_ENDPOINT_SECRET,
+
+  NATS_SERVERS: envVars.NATS_SERVERS,
 };
